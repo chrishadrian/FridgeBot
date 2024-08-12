67b03a77af149c1784af002b59bb965b5103ffde
chrishadrian <chrishadrianwong@gmail.com>
2024-08-12 00:35:28 -0700

    Modified folder structure

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -4,6 +4,11 @@ import { AI } from '@/lib/chat/actions'
 import { auth } from '@/auth'
 import { Session } from '@/lib/types'
 import { getMissingKeys } from '@/app/actions'
+import AcmeLogo from '@/components/ui/acme-logo';
+import { ArrowRightIcon } from '@heroicons/react/24/outline';
+import Link from 'next/link';
+import { lusitana } from '@/components/ui/fonts';
+import Image from 'next/image';
 
 export const metadata = {
   title: 'Next.js AI Chatbot'
@@ -13,6 +18,7 @@ export default async function IndexPage() {
   const id = nanoid()
   const session = (await auth()) as Session
   const missingKeys = await getMissingKeys()
+
 
   return (
     <AI initialAIState={{ chatId: id, messages: [] }}>
--- app/layout.tsx
+++ app/layout.tsx
@@ -8,57 +8,61 @@ import { Providers } from '@/components/providers'
 import { Header } from '@/components/header'
 import { Toaster } from '@/components/ui/sonner'
 
+import { UserProvider } from '@auth0/nextjs-auth0/client';
+
+
 export const metadata = {
-  metadataBase: process.env.VERCEL_URL
-    ? new URL(`https://${process.env.VERCEL_URL}`)
-    : undefined,
-  title: {
-    default: 'Next.js AI Chatbot',
-    template: `%s - Next.js AI Chatbot`
-  },
-  description: 'An AI-powered chatbot template built with Next.js and Vercel.',
-  icons: {
-    icon: '/favicon.ico',
-    shortcut: '/favicon-16x16.png',
-    apple: '/apple-touch-icon.png'
-  }
+	metadataBase: process.env.VERCEL_URL
+		? new URL(`https://${process.env.VERCEL_URL}`)
+		: undefined,
+	title: {
+		default: 'Next.js AI Chatbot',
+		template: `%s - Next.js AI Chatbot`
+	},
+	description: 'An AI-powered chatbot template built with Next.js and Vercel.',
+	icons: {
+		icon: '/favicon.ico',
+		shortcut: '/favicon-16x16.png',
+		apple: '/apple-touch-icon.png'
+	}
 }
 
 export const viewport = {
-  themeColor: [
-    { media: '(prefers-color-scheme: light)', color: 'white' },
-    { media: '(prefers-color-scheme: dark)', color: 'black' }
-  ]
+	themeColor: [
+		{ media: '(prefers-color-scheme: light)', color: 'white' },
+		{ media: '(prefers-color-scheme: dark)', color: 'black' }
+	]
 }
 
 interface RootLayoutProps {
-  children: React.ReactNode
+	children: React.ReactNode
 }
 
 export default function RootLayout({ children }: RootLayoutProps) {
-  return (
-    <html lang="en" suppressHydrationWarning>
-      <body
-        className={cn(
-          'font-sans antialiased',
-          GeistSans.variable,
-          GeistMono.variable
-        )}
-      >
-        <Toaster position="top-center" />
-        <Providers
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
-        >
-          <div className="flex flex-col min-h-screen">
-            <Header />
-            <main className="flex flex-col flex-1 bg-muted/50">{children}</main>
-          </div>
-          <TailwindIndicator />
-        </Providers>
-      </body>
-    </html>
-  )
+	return (
+		<html lang="en" suppressHydrationWarning>
+			<UserProvider>
+				<body
+					className={cn(
+						'font-sans antialiased',
+						GeistSans.variable,
+						GeistMono.variable
+					)}
+				>
+					<Toaster position="top-center" />
+					<Providers
+						attribute="class"
+						defaultTheme="system"
+						enableSystem
+						disableTransitionOnChange
+					>
+						<main>
+							{children}
+						</main>
+						<TailwindIndicator />
+					</Providers>
+				</body>
+			</UserProvider>
+		</html>
+	)
 }

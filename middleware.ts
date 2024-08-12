97e193b595ad635ca8ab5754d485d4b915e77c0b
chrishadrian <chrishadrianwong@gmail.com>
2024-08-12 00:35:50 -0700

    Created auth0 middleware and route handler

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,8 @@
-import NextAuth from 'next-auth'
-import { authConfig } from './auth.config'
+// middleware.js
+import { withMiddlewareAuthRequired } from '@auth0/nextjs-auth0/edge';
 
-export default NextAuth(authConfig).auth
+export default withMiddlewareAuthRequired();
 
 export const config = {
-  matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)']
-}
+  matcher: ['/dashboard/:path*', '/protected/:path*'], // Protect these routes
+};

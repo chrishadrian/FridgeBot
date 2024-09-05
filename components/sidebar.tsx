7cd92b6b662d31c6ced39c37960571ed1018637d
chrishadrian <chrishadrianwong@gmail.com>
2024-09-04 20:11:33 -0700

    Modified sidebar

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -4,58 +4,50 @@
 import {
   Card,
   Typography,
-  List,
-  ListItem,
-  ListItemPrefix,
-  ListItemSuffix,
   Chip,
 } from "@material-tailwind/react";
 import {
   PresentationChartBarIcon,
   ShoppingBagIcon,
   InboxIcon,
-	ChatBubbleBottomCenterTextIcon,
+  ChatBubbleBottomCenterTextIcon,
 } from "@heroicons/react/24/solid";
 import Image from "next/image";
- 
+import SidebarItem from "./sidebar-listitem";
+
+
 export function DefaultSidebar() {
   return (
     <Card className="h-screen w-full max-w-[20rem] p-4 shadow-2xl shadow-blue-gray-900/5" placeholder={undefined} onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined}>
       <div className="mb-2 flex items-center gap-4 p-4">
-				<Image src="/next.svg" alt="brand"  width={32} height={32}/>
+        <Image src="/next.svg" alt="brand" width={32} height={32} />
         <Typography variant="h5" color="blue-gray" placeholder={undefined} onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined}>
           FridgeBot
         </Typography>
       </div>
-      <List placeholder={undefined} onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined}>
-        <ListItem placeholder={undefined} onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined}>
-          <ListItemPrefix placeholder={undefined} onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined}>
-            <PresentationChartBarIcon className="size-5 mr-1" />
-          </ListItemPrefix>
-          Dashboard
-        </ListItem>
-        <ListItem placeholder={undefined} onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined}>
-          <ListItemPrefix placeholder={undefined} onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined}>
-            <ChatBubbleBottomCenterTextIcon className="size-5 mr-1" />
-          </ListItemPrefix>
-          FridgeChat
-        </ListItem>
-				<ListItem placeholder={undefined} onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined}>
-          <ListItemPrefix placeholder={undefined} onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined}>
-            <ShoppingBagIcon className="size-5 mr-1" />
-          </ListItemPrefix>
-          Inventory
-        </ListItem>
-        <ListItem placeholder={undefined} onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined}>
-          <ListItemPrefix placeholder={undefined} onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined}>
-            <InboxIcon className="size-5 mr-1" />
-          </ListItemPrefix>
-          Inbox
-          <ListItemSuffix placeholder={undefined} onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined}>
-            <Chip value="14" size="sm" variant="ghost" color="blue-gray" className="rounded-full" />
-          </ListItemSuffix>
-        </ListItem>
-      </List>
+      <ul>
+        <SidebarItem
+          href="/dashboard"
+          icon={PresentationChartBarIcon}
+          text="Dashboard"
+        />
+        <SidebarItem
+          href="/chat"
+          icon={ChatBubbleBottomCenterTextIcon}
+          text="FridgeChat"
+        />
+        <SidebarItem
+          href="/inventory"
+          icon={ShoppingBagIcon}
+          text="Inventory"
+        />
+        <SidebarItem
+          href="/inbox"
+          icon={InboxIcon}
+          text="Inbox"
+          suffix={<Chip value="14" size="sm" variant="ghost" color="blue-gray" className="rounded-full" />}
+        />
+      </ul>
     </Card>
   );
 }
